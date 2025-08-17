6d419bd8b0d9bb7a71265b4fe2ed2d3af030db8b
Helen <kozovyakina@gmail.com>
2025-08-17 16:37:51 +0300

    auth

--- third/todos/todos/urls.py
+++ third/todos/todos/urls.py
@@ -23,7 +23,11 @@ urlpatterns = [
 
     # Auth
     path('signup/', views.signup_user, name='signupuser'),
+    path('logout/', views.logout_user, name='logoutuser'),
+    path('login/', views.login_user, name='loginuser'),
 
     # Todos
     path('current/', views.current_todos, name='currenttodos'),
+    path('', views.home, name='home'),
+    path('create/', views.create_todo, name='createtodo'),
 ]
